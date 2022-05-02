268f71a7499c76f7df9fded68534b7cc368de54f
user-miner1 <amvnews@mail.ru>
2022-05-03 02:32:09 +0300

    setup.py was added

---
+++ setup.py
@@ -0,0 +1,17 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='classification_models_3D',
+    version='1.0.4',
+    author='Roman Sol (ZFTurbo)',
+    packages=['classification_models_3D', 'classification_models_3D/models'],
+    url='https://github.com/ZFTurbo/classification_models_3D',
+    description='Set of models for classification of 3D volumes.',
+    long_description='3D variants of popular CNN models for classification like ResNets, DenseNets, VGG, etc. '
+                     'It also contains weights obtained by converting ImageNet weights from the same 2D models. '
+                     'Models work with keras and tensorflow.keras.'
+                     'More details: https://github.com/ZFTurbo/classification_models_3D',
+)
