1a37ae0fc4ae9a75049edca77bfe58eb98e98cf4
Combaret <nicolas.combaret@thermofisher.com>
2024-04-29 11:16:30 +0200

    Improve and clean tst_keras_tensorflow.py
    This test now computes the memory usage of the tested models.
    It also makes sure that the other backends are not visible in sys.modules
    (if they are imported, an exception will be thrown).
    The list of tested models is directly given by the Classifiers class, which
    ensures that all the models that the factory can build are tested.

--- tst_keras_tensorflow.py
+++ tst_keras_tensorflow.py
@@ -3,12 +3,52 @@ __author__ = 'ZFTurbo: https://kaggle.com/zfturbo'
 
 
 if __name__ == '__main__':
+    import sys
     import os
 
-    gpu_use = 4
-    print('GPU use: {}'.format(gpu_use))
+    # For this test, make sure that only the tested framework is available
+    sys.modules['jax'] = None
+    sys.modules['torch'] = None
+
+    gpu_use = 0
+    print(f"GPU use: {gpu_use}")
     os.environ["KERAS_BACKEND"] = "tensorflow"
-    os.environ["CUDA_VISIBLE_DEVICES"] = "{}".format(gpu_use)
+    os.environ["CUDA_VISIBLE_DEVICES"] = f"{gpu_use}"
+
+
+def get_model_memory_usage(batch_size, model):
+    import numpy as np
+    from keras import backend as K
+    from keras.src.utils import summary_utils
+
+    shapes_mem_count = 0
+    internal_model_mem_count = 0
+    for l in model.layers:
+        layer_type = l.__class__.__name__
+        if layer_type == 'Model':
+            internal_model_mem_count += get_model_memory_usage(batch_size, l)
+        single_layer_mem = 1
+        out_shape = l.output.shape
+        if type(out_shape) is list:
+            out_shape = out_shape[0]
+        for s in out_shape:
+            if s is None:
+                continue
+            single_layer_mem *= s
+        shapes_mem_count += single_layer_mem
+
+    trainable_count = summary_utils.count_params(model.trainable_weights)
+    non_trainable_count = summary_utils.count_params(model.non_trainable_weights)
+
+    number_size = 4.0
+    if K.floatx() == 'float16':
+        number_size = 2.0
+    if K.floatx() == 'float64':
+        number_size = 8.0
+
+    total_memory = number_size * (batch_size * shapes_mem_count + trainable_count + non_trainable_count)
+    gbytes = np.round(total_memory / (1024.0 ** 3), 3) + internal_model_mem_count
+    return gbytes
 
 
 def tst_keras():
@@ -17,24 +57,15 @@ def tst_keras():
     from keras import backend as K
     from classification_models_3D.kkeras import Classifiers
 
-
-    print('Keras version: {}'.format(__version__))
+    print(f"Keras version {__version__} using {K.backend()} backend")
     include_top = False
-    # use_weights = 'imagenet'
     use_weights = None
-    list_of_models = [
-        'resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152', 'seresnet18', 'seresnet34', 'seresnet50',
-        'seresnet101', 'seresnet152', 'seresnext50', 'seresnext101', 'senet154', 'resnext50', 'resnext101',
-        'vgg16', 'vgg19', 'densenet121', 'densenet169', 'densenet201', 'mobilenet', 'mobilenetv2',
-        'inceptionresnetv2', 'inceptionv3',  'efficientnetb0', 'efficientnetb1', 'efficientnetb2', 'efficientnetb3',
-        'efficientnetb4', 'efficientnetb5', 'efficientnetb6', 'efficientnetb7', 'efficientnetv2-b0',
-        'efficientnetv2-b1', 'efficientnetv2-b2', 'efficientnetv2-b3', 'efficientnetv2-s', 'efficientnetv2-m',
-        'efficientnetv2-l'
-    ]
+    list_of_models = Classifiers.models_names()
     for type in list_of_models:
-        modelPoint, preprocess_input = Classifiers.get(type)
+        modelPoint, _ = Classifiers.get(type)
         model = modelPoint(input_shape=(128, 128, 128, 3), include_top=include_top, weights=use_weights)
         print(model.summary())
+        print(get_model_memory_usage(1, model), 'GB')
         K.clear_session()
 
 
