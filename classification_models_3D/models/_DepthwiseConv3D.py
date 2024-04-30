5cee2276865d8543423f0947a75ec0f85751d4a0
Combaret <nicolas.combaret@thermofisher.com>
2024-04-30 10:40:21 +0200

    Adapt DepthwiseConv3D
    There were more work to do for this class.
    - imports were adapted to the new API and tf.keras imports were removed
      since it is recommended not to use it. Some functions that were defined
      in the tf.keras case are kept alive because they were removed from keras
      (conv3d_args_preprocessor and generate_legacy_interface).
    - tensorflow calls were replaced by keras.ops calls but few changes were
      necessary:
        - self._strides is removed and replaced by self.strides. strides
          parameter of keras.ops.conv expects a tuple with 1, 2, or 3 elements.
        - self._data_format is removed and replaced by self.data_format. keras
          does not use "NDHWC" and "NCDHW" data format but "channel_first" and
          "channel_last".
    - keras.utils.conv_utils.conv_output_length does not exist anymore
      therefore the function was copied from the keras repository.
    - self.depthwise_kernel member variable was renamed self._kernel because
      recent changes in keras>=3.3 somehow requires that naming. Another option
      is to override two BaseConv methods (save_own_variables and
      load_own_variables).

--- classification_models_3D/models/_DepthwiseConv3D.py
+++ classification_models_3D/models/_DepthwiseConv3D.py
@@ -1,174 +1,110 @@
 # Implementation: https://github.com/alexandrosstergiou/keras-DepthwiseConv3D
 
-import tensorflow as tf
-try:
-    from keras import backend as K
-    from keras import initializers
-    from keras import regularizers
-    from keras import constraints
-    from keras import layers
-    from keras.engine import InputSpec
-    from keras.legacy.interfaces import conv3d_args_preprocessor, generate_legacy_interface
-    from keras.layers import Conv3D
-    from keras.backend.tensorflow_backend import _preprocess_padding, _preprocess_conv3d_input
-    from keras.utils import conv_utils
-except:
-    from tensorflow.keras import backend as K
-    from tensorflow.keras import initializers
-    from tensorflow.keras import regularizers
-    from tensorflow.keras import constraints
-    from tensorflow.keras import layers
-    from tensorflow.keras.layers import Conv3D
-    from tensorflow.keras.layers import InputSpec
-    # from tensorflow.keras.utils import conv_utils
-    import tensorflow.keras.utils as conv_utils
-    import six
-    import warnings
-    from distutils.version import StrictVersion
+import keras.ops as kops
+from keras import backend as K
+from keras import initializers
+from keras import regularizers
+from keras import constraints
+from keras import layers
+from keras import InputSpec
+from keras.src.legacy.backend import _preprocess_padding, _preprocess_conv3d_input
 
 
-    def generate_legacy_interface(allowed_positional_args=None,
-                                  conversions=None,
-                                  preprocessor=None,
-                                  value_conversions=None):
-        allowed_positional_args = allowed_positional_args or []
-        conversions = conversions or []
-        value_conversions = value_conversions or []
+def generate_legacy_interface(allowed_positional_args=None,
+                                conversions=None,
+                                preprocessor=None,
+                                value_conversions=None):
+    allowed_positional_args = allowed_positional_args or []
+    conversions = conversions or []
+    value_conversions = value_conversions or []
 
-        def legacy_support(func):
-            @six.wraps(func)
-            def wrapper(*args, **kwargs):
-                layer_name = args[0].__class__.__name__
-                if preprocessor:
-                    args, kwargs, converted = preprocessor(args, kwargs)
-                else:
-                    converted = []
-                if len(args) > len(allowed_positional_args) + 1:
-                    raise TypeError('Layer `' + layer_name +
-                                    '` can accept only ' +
-                                    str(len(allowed_positional_args)) +
-                                    ' positional arguments (' +
-                                    str(allowed_positional_args) + '), but '
-                                                                   'you passed the following '
-                                                                   'positional arguments: ' +
-                                    str(args[1:]))
-                for key in value_conversions:
-                    if key in kwargs:
-                        old_value = kwargs[key]
-                        if old_value in value_conversions[key]:
-                            kwargs[key] = value_conversions[key][old_value]
-                for old_name, new_name in conversions:
-                    if old_name in kwargs:
-                        value = kwargs.pop(old_name)
-                        kwargs[new_name] = value
-                        converted.append((new_name, old_name))
-                if converted:
-                    signature = '`' + layer_name + '('
-                    for value in args[1:]:
-                        if isinstance(value, six.string_types):
-                            signature += '"' + value + '"'
-                        else:
-                            signature += str(value)
+    def legacy_support(func):
+        @six.wraps(func)
+        def wrapper(*args, **kwargs):
+            layer_name = args[0].__class__.__name__
+            if preprocessor:
+                args, kwargs, converted = preprocessor(args, kwargs)
+            else:
+                converted = []
+            if len(args) > len(allowed_positional_args) + 1:
+                raise TypeError('Layer `' + layer_name +
+                                '` can accept only ' +
+                                str(len(allowed_positional_args)) +
+                                ' positional arguments (' +
+                                str(allowed_positional_args) + '), but '
+                                                                'you passed the following '
+                                                                'positional arguments: ' +
+                                str(args[1:]))
+            for key in value_conversions:
+                if key in kwargs:
+                    old_value = kwargs[key]
+                    if old_value in value_conversions[key]:
+                        kwargs[key] = value_conversions[key][old_value]
+            for old_name, new_name in conversions:
+                if old_name in kwargs:
+                    value = kwargs.pop(old_name)
+                    kwargs[new_name] = value
+                    converted.append((new_name, old_name))
+            if converted:
+                signature = '`' + layer_name + '('
+                for value in args[1:]:
+                    if isinstance(value, six.string_types):
+                        signature += '"' + value + '"'
+                    else:
+                        signature += str(value)
+                    signature += ', '
+                for i, (name, value) in enumerate(kwargs.items()):
+                    signature += name + '='
+                    if isinstance(value, six.string_types):
+                        signature += '"' + value + '"'
+                    else:
+                        signature += str(value)
+                    if i < len(kwargs) - 1:
                         signature += ', '
-                    for i, (name, value) in enumerate(kwargs.items()):
-                        signature += name + '='
-                        if isinstance(value, six.string_types):
-                            signature += '"' + value + '"'
-                        else:
-                            signature += str(value)
-                        if i < len(kwargs) - 1:
-                            signature += ', '
-                    signature += ')`'
-                    warnings.warn('Update your `' + layer_name +
-                                  '` layer call to the Keras 2 API: ' + signature)
-                return func(*args, **kwargs)
+                signature += ')`'
+                warnings.warn('Update your `' + layer_name +
+                                '` layer call to the Keras 2 API: ' + signature)
+            return func(*args, **kwargs)
 
-            return wrapper
+        return wrapper
 
-        return legacy_support
+    return legacy_support
 
 
-    def conv3d_args_preprocessor(args, kwargs):
-        if len(args) > 5:
-            raise TypeError('Layer can receive at most 4 positional arguments.')
-        if len(args) == 5:
-            if isinstance(args[2], int) and isinstance(args[3], int) and isinstance(args[4], int):
-                kernel_size = (args[2], args[3], args[4])
-                args = [args[0], args[1], kernel_size]
-        elif len(args) == 4 and isinstance(args[3], int):
-            if isinstance(args[2], int) and isinstance(args[3], int):
-                new_keywords = ['padding', 'strides', 'data_format']
-                for kwd in new_keywords:
-                    if kwd in kwargs:
-                        raise ValueError(
-                            'It seems that you are using the Keras 2 '
-                            'and you are passing both `kernel_size` and `strides` '
-                            'as integer positional arguments. For safety reasons, '
-                            'this is disallowed. Pass `strides` '
-                            'as a keyword argument instead.')
-            if 'kernel_dim3' in kwargs:
-                kernel_size = (args[2], args[3], kwargs.pop('kernel_dim3'))
-                args = [args[0], args[1], kernel_size]
-        elif len(args) == 3:
-            if 'kernel_dim2' in kwargs and 'kernel_dim3' in kwargs:
-                kernel_size = (args[2],
-                               kwargs.pop('kernel_dim2'),
-                               kwargs.pop('kernel_dim3'))
-                args = [args[0], args[1], kernel_size]
-        elif len(args) == 2:
-            if 'kernel_dim1' in kwargs and 'kernel_dim2' in kwargs and 'kernel_dim3' in kwargs:
-                kernel_size = (kwargs.pop('kernel_dim1'),
-                               kwargs.pop('kernel_dim2'),
-                               kwargs.pop('kernel_dim3'))
-                args = [args[0], args[1], kernel_size]
-        return args, kwargs, [('kernel_size', 'kernel_dim*')]
-
-
-    def _preprocess_padding(padding):
-        """Convert keras' padding to tensorflow's padding.
-
-        # Arguments
-            padding: string, `"same"` or `"valid"`.
-
-        # Returns
-            a string, `"SAME"` or `"VALID"`.
-
-        # Raises
-            ValueError: if `padding` is invalid.
-        """
-        if padding == 'same':
-            padding = 'SAME'
-        elif padding == 'valid':
-            padding = 'VALID'
-        else:
-            raise ValueError('Invalid padding: ' + str(padding))
-        return padding
-
-
-    def dtype(x):
-        return x.dtype.base_dtype.name
-
-
-    def _has_nchw_support():
-        return True
-
-
-    def _preprocess_conv3d_input(x, data_format):
-        """Transpose and cast the input before the conv3d.
-
-        # Arguments
-            x: input tensor.
-            data_format: string, `"channels_last"` or `"channels_first"`.
-
-        # Returns
-            A tensor.
-        """
-        # tensorflow doesn't support float64 for conv layer before 1.8.0
-        if (dtype(x) == 'float64' and
-                StrictVersion(tf.__version__.split('-')[0]) < StrictVersion('1.8.0')):
-            x = tf.cast(x, 'float32')
-        tf_data_format = 'NDHWC'
-        return x, tf_data_format
+def conv3d_args_preprocessor(args, kwargs):
+    if len(args) > 5:
+        raise TypeError('Layer can receive at most 4 positional arguments.')
+    if len(args) == 5:
+        if isinstance(args[2], int) and isinstance(args[3], int) and isinstance(args[4], int):
+            kernel_size = (args[2], args[3], args[4])
+            args = [args[0], args[1], kernel_size]
+    elif len(args) == 4 and isinstance(args[3], int):
+        if isinstance(args[2], int) and isinstance(args[3], int):
+            new_keywords = ['padding', 'strides', 'data_format']
+            for kwd in new_keywords:
+                if kwd in kwargs:
+                    raise ValueError(
+                        'It seems that you are using the Keras 2 '
+                        'and you are passing both `kernel_size` and `strides` '
+                        'as integer positional arguments. For safety reasons, '
+                        'this is disallowed. Pass `strides` '
+                        'as a keyword argument instead.')
+        if 'kernel_dim3' in kwargs:
+            kernel_size = (args[2], args[3], kwargs.pop('kernel_dim3'))
+            args = [args[0], args[1], kernel_size]
+    elif len(args) == 3:
+        if 'kernel_dim2' in kwargs and 'kernel_dim3' in kwargs:
+            kernel_size = (args[2],
+                            kwargs.pop('kernel_dim2'),
+                            kwargs.pop('kernel_dim3'))
+            args = [args[0], args[1], kernel_size]
+    elif len(args) == 2:
+        if 'kernel_dim1' in kwargs and 'kernel_dim2' in kwargs and 'kernel_dim3' in kwargs:
+            kernel_size = (kwargs.pop('kernel_dim1'),
+                            kwargs.pop('kernel_dim2'),
+                            kwargs.pop('kernel_dim3'))
+            args = [args[0], args[1], kernel_size]
+    return args, kwargs, [('kernel_size', 'kernel_dim*')]
 
 
 def depthwise_conv3d_args_preprocessor(args, kwargs):
@@ -308,8 +244,6 @@ class DepthwiseConv3D(layers.Conv3D):
         self.bias_initializer = initializers.get(bias_initializer)
         self.dilation_rate = dilation_rate
         self._padding = _preprocess_padding(self.padding)
-        self._strides = (1,) + self.strides + (1,)
-        self._data_format = "NDHWC"
         self.input_dim = None
 
     def build(self, input_shape):
@@ -341,7 +275,7 @@ class DepthwiseConv3D(layers.Conv3D):
                                   self.input_dim,
                                   self.depth_multiplier)
 
-        self.depthwise_kernel = self.add_weight(
+        self._kernel = self.add_weight(
             shape=depthwise_kernel_shape,
             initializer=self.depthwise_initializer,
             name='depthwise_kernel',
@@ -363,26 +297,21 @@ class DepthwiseConv3D(layers.Conv3D):
     def call(self, inputs, training=None):
         inputs = _preprocess_conv3d_input(inputs, self.data_format)
 
-        if self.data_format == 'channels_last':
-            dilation = (1,) + self.dilation_rate + (1,)
-        else:
-            dilation = self.dilation_rate + (1,) + (1,)
-
-        if self._data_format == 'NCDHW':
-            outputs = tf.concat(
-                [tf.nn.conv3d(inputs[0][:, i:i+self.input_dim//self.groups, :, :, :], self.depthwise_kernel[:, :, :, i:i+self.input_dim//self.groups, :],
-                    strides=self._strides,
+        if self.data_format == 'channel_first':
+            outputs = kops.concatenate(
+                [kops.conv(inputs[0][:, i:i+self.input_dim//self.groups, :, :, :], self._kernel[:, :, :, i:i+self.input_dim//self.groups, :],
+                    strides=self.strides,
                     padding=self._padding,
-                    dilations=dilation,
-                    data_format=self._data_format) for i in range(0, self.input_dim, self.input_dim//self.groups)], axis=1)
+                    dilation_rate=self.dilation_rate,
+                    data_format=self.data_format) for i in range(0, self.input_dim, self.input_dim//self.groups)], axis=1)
 
         else:
-            outputs = tf.concat(
-                [tf.nn.conv3d(inputs[0][:, :, :, :, i:i+self.input_dim//self.groups], self.depthwise_kernel[:, :, :, i:i+self.input_dim//self.groups, :],
-                    strides=self._strides,
+            outputs = kops.concatenate(
+                [kops.conv(inputs[0][:, :, :, :, i:i+self.input_dim//self.groups], self._kernel[:, :, :, i:i+self.input_dim//self.groups, :],
+                    strides=self.strides,
                     padding=self._padding,
-                    dilations=dilation,
-                    data_format=self._data_format) for i in range(0, self.input_dim, self.input_dim//self.groups)], axis=-1)
+                    dilation_rate=self.dilation_rate,
+                    data_format=self.data_format) for i in range(0, self.input_dim, self.input_dim//self.groups)], axis=-1)
 
         if self.bias is not None:
             outputs = K.bias_add(
@@ -396,6 +325,31 @@ class DepthwiseConv3D(layers.Conv3D):
         return outputs
 
     def compute_output_shape(self, input_shape):
+        def conv_output_length(input_length, filter_size, padding, stride, dilation=1):
+            """Determines output length of a convolution given input length.
+
+            Args:
+                input_length: integer.
+                filter_size: integer.
+                padding: one of "same", "valid", "full", "causal"
+                stride: integer.
+                dilation: dilation rate, integer.
+
+            Returns:
+                The output length (integer).
+            """
+            if input_length is None:
+                return None
+            assert padding in {"same", "valid", "full", "causal"}
+            dilated_filter_size = filter_size + (filter_size - 1) * (dilation - 1)
+            if padding in ["same", "causal"]:
+                output_length = input_length
+            elif padding == "valid":
+                output_length = input_length - dilated_filter_size + 1
+            elif padding == "full":
+                output_length = input_length + dilated_filter_size - 1
+            return (output_length + stride - 1) // stride
+
         if self.data_format == 'channels_first':
             depth = input_shape[2]
             rows = input_shape[3]
@@ -407,15 +361,15 @@ class DepthwiseConv3D(layers.Conv3D):
             cols = input_shape[3]
             out_filters = self.groups * self.depth_multiplier
 
-        depth = conv_utils.conv_output_length(depth, self.kernel_size[0],
+        depth = conv_output_length(depth, self.kernel_size[0],
                                              self.padding,
                                              self.strides[0])
 
-        rows = conv_utils.conv_output_length(rows, self.kernel_size[1],
+        rows = conv_output_length(rows, self.kernel_size[1],
                                              self.padding,
                                              self.strides[1])
 
-        cols = conv_utils.conv_output_length(cols, self.kernel_size[2],
+        cols = conv_output_length(cols, self.kernel_size[2],
                                              self.padding,
                                              self.strides[2])
 
