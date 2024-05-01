6e4a8e0158f3cceb6516f7566523cf776a321ab7
user-miner1 <amvnews@mail.ru>
2024-05-01 14:34:46 +0300

    Keras 3 support big update. Prepare 1.1.0 release

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ except ImportError:
 
 setup(
     name='classification_models_3D',
-    version='1.0.10',
+    version='1.1.0',
     author='Roman Sol (ZFTurbo)',
     packages=['classification_models_3D', 'classification_models_3D/models'],
     url='https://github.com/ZFTurbo/classification_models_3D',
     description='Set of models for classification of 3D volumes.',
-    long_description='3D variants of popular CNN models for classification like ResNets, DenseNets, VGG, etc. '
+    long_description='This repository contains 3D variants of popular classification CNN models like ResNets, DenseNets, VGG, etc for keras module. '
                      'It also contains weights obtained by converting ImageNet weights from the same 2D models. '
-                     'Models work with keras and tensorflow.keras.'
+                     'Models work with keras 3 with different backends like Tensorflow, Torch and Jax.'
                      'More details: https://github.com/ZFTurbo/classification_models_3D',
 )
